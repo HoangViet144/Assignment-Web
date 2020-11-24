ca3c37eaa035d5a0dcf49c82184e16b6affc26ca
TranHoangViet <viet14042000@gmail.com>
2020-11-24 19:51:58 +0700

    finish index.html webview

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("background").style.width = "0";
 }
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
