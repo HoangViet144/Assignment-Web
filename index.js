db85825af8f4a896ed4a45d62b72054ae4f0d8f1
TranHoangViet <viet14042000@gmail.com>
2020-11-24 14:33:53 +0700

    finish header

---
+++ index.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("background").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("background").style.width = "0";
+}
