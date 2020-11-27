8c9eed2d93751cb23bc0132d27032e00449df85b
TranHoangViet <viet14042000@gmail.com>
2020-11-27 10:00:07 +0700

    update menu dropdown

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
+    var mySidebar = document.getElementById("mySidebar");
+    mySidebar.style.width = "300px";
     document.getElementById("background").style.width = "100%";
+    mySidebar.style.border = '1px solid black';
 }
 
 function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
+    var mySidebar = document.getElementById("mySidebar");
+    mySidebar.style.width = "0";
+    mySidebar.style.border = "none";
     document.getElementById("background").style.width = "0";
 }
 function topFunction() {
