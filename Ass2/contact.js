83e9111227af189b4e443a351cc7a3001b55b90d
TranHoangViet <viet14042000@gmail.com>
2020-12-28 22:04:13 +0700

    edit, remove post/cmt

--- Ass2/contact.js
+++ Ass2/contact.js
@@ -56,17 +56,26 @@ $(document).ready(function () {
             postsHTML += "</div>" +
                 "</div>"
             for (var id in posts) {
-                var roleClass = (posts[id].userrole == 1 ? 'admin' : 'user')
+                var roleClass = (posts[id].userrole === 3 ? 'admin' : 'user')
 
                 var row =
                     "<div class='post'>" +
                     "<div class='article'>" +
                     `<span class='post_name ${roleClass}'>${posts[id].username}</span>` +
-                    `<span class='post_content'>${posts[id].content}</span>` +
+                    `<span class='post_content post_content_${id}'>${posts[id].content}</span>` +
                     "</div>" +
                     "<div class='post_button'>" +
-                    `<button class='make_cmt' id='cmt_btn_${id}'>Bình luận</button>` +
-                    "</div>" +
+                    `<button class='make_cmt' id='cmt_btn_${id}'>Bình luận</button>`
+                if (posts[id].userid === result['userID']) {
+                    row += `<button class='edit_post' id='edit_btn_${id}'>Sửa</button>` +
+                        `<button class='delete_post' id='delete_btn_${id}'>Xóa</button>`
+                }
+                else if (+result['role'] === 3) {
+                    row +=
+                        `<button class='delete_post' id='delete_btn_${id}'>Xóa</button>`
+                }
+
+                row += "</div>" +
                     `<div class='comments comment_${id}'>` +
                     "<div class='comment_new'>" +
                     `<span class='post_name ${userrole}'>${username}</span>`
@@ -78,12 +87,15 @@ $(document).ready(function () {
                         `<span><button class='send_btn' id='send_${id}'>Gửi</button></span>` +
                         "</div>"
                 for (var comment of posts[id].comments) {
-                    var roleClass = (comment.userrole == 3 ? 'admin' : 'user')
+                    var roleClass = (comment.userrole === 3 ? 'admin' : 'user')
                     row +=
                         `<div class='comment'>` +
                         `<span class='post_name ${roleClass}'>${comment.username}</span>` +
-                        `<span class='post_content'>${comment.content}</span>` +
-                        "</div>"
+                        `<span class='post_content'>${comment.content}</span>`
+                    if (+result['role'] === 3 || comment.userid === result['userID']) {
+                        row += `<span class='delete_cmt'><button class='delete_cmt_btn' id='btn_${comment.id}' >Xóa</button></span>`
+                    }
+                    row += "</div>"
                 }
                 row +=
                     "</div >" +
@@ -128,4 +140,77 @@ $(document).on('click', '.send_btn', function () {
             window.location.href = 'contact.php'
         }
     })
+})
+$(document).on('click', '.delete_post', function () {
+    var id = $(this).attr("id").substring(11)
+    console.log(id)
+    var result = confirm("Bạn có muốn xóa nội dung này?");
+    if (result) {
+        $.ajax({
+            type: 'DELETE',
+            url: './service/post.php/' + id,
+            success: function (response) {
+                alert(response)
+                window.location.href = 'contact.php';
+            }
+        })
+    }
+})
+$(document).on('click', '.edit_post', function () {
+    var id = $(this).attr("id").substring(9)
+    var buttonDiv = 'edit_btn_' + id
+    document.getElementById(buttonDiv).disabled = true;
+    var contentDiv = ".post_content_" + id
+    var content = $(contentDiv).html()
+    console.log(content, contentDiv)
+    var editDiv =
+        `<span class='post_content post_content_${id} textarea_cmt'>` +
+        `<textarea class='post_content_textarea_${id}'>${content}</textarea>` +
+        "</span>" +
+        "<span>" +
+        `<button class='save' id='save_${id}'>Lưu</button>` +
+        "</span>"
+    $(contentDiv).replaceWith(editDiv)
+
+})
+$(document).on('click', '.save', function () {
+    var id = $(this).attr("id").substring(5)
+    var contentDiv = ".post_content_textarea_" + id
+    var content = $(contentDiv).val()
+
+    $.ajax({
+        type: 'PUT',
+        url: '/service/post.php',
+        dataType: 'json',
+        data: {
+            id: id,
+            content: content,
+        },
+        success: function (response) {
+            alert(response)
+            window.location.href = 'contact.php'
+        }
+    })
+})
+$(document).on('click', '.delete_cmt_btn', function () {
+    var id = $(this).attr("id").substring(4)
+    console.log(id)
+    var result = confirm("Bạn có muốn xóa nội dung này?");
+    var cookie = document.cookie
+    cookie = cookie.split('; ');
+    var result = {};
+    for (var i = 0; i < cookie.length; i++) {
+        var cur = cookie[i].split('=');
+        result[cur[0]] = cur[1];
+    }
+    if (result) {
+        $.ajax({
+            type: 'DELETE',
+            url: './service/post.php/' + id,
+            success: function (response) {
+                alert(response)
+                window.location.href = 'contact.php';
+            }
+        })
+    }
 })
--- Ass2/service/post.php
+++ Ass2/service/post.php
@@ -33,7 +33,48 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $query = "INSERT INTO posts (userid,content,postid,`order`) VALUES ($userid,'$content',$postID,$order);";
     $result = mysqli_query($con, $query);
     if ($result === TRUE) {
+        mysqli_close($con);
         echo json_encode("Them thanh cong");
+    } else {
+        echo json_encode("Loi: " . $con->error);
+        mysqli_close($con);
+    }
+}
+if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+    include "../config.php";
+    $params = explode('/', trim($_SERVER['PATH_INFO'], '/'));
+
+    if (!isset($params[0])) {
+        mysqli_close($con);
+        echo json_encode("Loi");
+        return;
+    }
+
+    $result = mysqli_query($con, "DELETE FROM posts WHERE id = {$params[0]} OR postID = {$params[0]}");
+
+
+    if ($result === TRUE) {
+        mysqli_close($con);
+        echo json_encode('Xoa thanh cong');
+    } else {
+        echo json_encode("Loi: " . $con->error);
+        mysqli_close($con);
+    }
+}
+if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+    parse_str(file_get_contents('php://input'), $_PUT);
+    $id = $_PUT['id'];
+    $content = $_PUT['content'];
+
+    if ($content == '') {
+        echo json_encode('Noi dung cap nhat rong');
+        return;
+    }
+    include "../config.php";
+    $result = mysqli_query($con, "UPDATE posts SET content='$content' WHERE id='$id'");
+
+    if ($result === TRUE) {
+        echo json_encode("Cap nhat thanh cong");
         mysqli_close($con);
     } else {
         echo json_encode("Loi: " . $con->error);
